aa8e89af448449b8a2a5e65ef6e6e06f5db287e4
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-29 07:34:01 -0300

    feat: func get_data()

--- scraping_the_world/routes/consult.py
+++ scraping_the_world/routes/consult.py
@@ -1,4 +1,17 @@
+from urllib.parse import urlparse
+
 from flask_restful import Resource, reqparse
+
+from scraping_the_world.scrapers.americanas import scraping_americanas
+
+
+def get_data(url):
+    parse = urlparse(url)
+    hostname = parse.hostname
+
+    if 'americanas' in hostname:
+        result = scraping_americanas(url)
+        return result
 
 
 class Consult(Resource):
@@ -8,6 +21,10 @@ class Consult(Resource):
     def get(self):
         request_data = Consult.params.parse_args()
         print(request_data)
+
+        result = get_data(request_data['url'])
+        print(result)
+
         json_return = {
             'titulo': 'esse e o titulo',
             'imagem': 'esse e a imagem',
