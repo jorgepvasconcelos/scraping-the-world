a40a95832aa2717b1e7d3c649aa1cf639c0c64d0
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-29 07:34:59 -0300

    feat: func get_driver()

--- scraping_the_world/scrapers/americanas.py
+++ scraping_the_world/scrapers/americanas.py
@@ -1,12 +1,33 @@
-import requests
-from requests_html import HTMLSession
-session = HTMLSession()
-
-url = 'https://www.saraiva.com.br/box-o-essencial-da-psicologia-3-volumes-10081856/p'
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
 
 
-response = session.get(url)
-response.html.render(reload=True)
-# response = requests.get(url=url)
+def get_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-ssl-errors=yes')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--headless')
 
-print(response.text)
+    driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', options=options)
+    driver.maximize_window()
+
+    return driver
+
+
+def scraping_americanas(url):
+    print("Test Execution Started")
+
+    driver = get_driver()
+    driver.get(url)
+    sleep(10)
+    var = driver.find_element(By.CSS_SELECTOR, '.priceSales').text
+
+    driver.close()
+    driver.quit()
+    return var
+
+
+if __name__ == '__main__':
+    scraping_result = scraping_americanas('https://www.americanas.com.br/produto/3068486001')
+    print(scraping_result)
