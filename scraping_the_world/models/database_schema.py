34e07ba3f5c6fdab997d0c29186a4f8a81b4b189
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-29 17:21:37 -0300

    feat: change field increase size of collums

--- scraping_the_world/models/database_schema.py
+++ scraping_the_world/models/database_schema.py
@@ -3,10 +3,10 @@ create table if not exists sites_data
 (
 	id int auto_increment
 		primary key,
-	titulo varchar(100) null,
+	titulo varchar(200) null,
 	preco varchar(50) null,
-	imagem varchar(50) null,
-	descricao varchar(500) null,
+	imagem varchar(100) null,
+	descricao varchar(1000) null,
 	url_recebida varchar(500) null,
 	data_verificado datetime default current_timestamp() null,
 	url_site varchar(500) null
