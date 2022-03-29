da4d98510d81c9d32853522ea900cd19d2443f43
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-28 21:13:34 -0300

    feat: request params and response body

--- scraping_the_world/routes/consult.py
+++ scraping_the_world/routes/consult.py
@@ -1,6 +1,17 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 
 class Consult(Resource):
+    params = reqparse.RequestParser()
+    params.add_argument('url', type=str, required=True, help="The field 'url' cannot be left blank.")
+
     def get(self):
-        return 'Consult'
+        request_data = Consult.params.parse_args()
+        print(request_data)
+        json_return = {
+            'titulo': 'esse e o titulo',
+            'imagem': 'esse e a imagem',
+            'preco': 'esse e o preco',
+            'url': 'esse e a url',
+        }
+        return json_return
