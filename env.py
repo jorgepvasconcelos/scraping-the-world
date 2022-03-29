c64f6aaa4a4f0fa94ac7da58c6035ddc5194add9
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-29 19:11:56 -0300

    feat: env working

--- env.py
+++ env.py
@@ -1,25 +1,32 @@
 from dotenv import dotenv_values
 
-__ON_DOCKER = True
+try:
+    __env_type = dotenv_values('.env')
+    __env_type = __env_type['ENV']
+    __ON_DOCKER = True
+except:
+    __env_type = dotenv_values('../.env')
+    __env_type = __env_type['ENV']
+    __ON_DOCKER = False
 
 if __ON_DOCKER:
-    ENV = dotenv_values('.env')
+    if __env_type == 'PROD':
+        print('============ ENV PROD ===========')
+        ENV = dotenv_values('./envs/.env.prod')
+    elif __env_type == 'HOMOLOG':
+        print('============ ENV HOMOLOG ===========')
+        ENV = dotenv_values('./envs/.env.homolog')
+    elif __env_type == 'DEV':
+        print('============ ENV DEV ===========')
+        ENV = dotenv_values('./envs/.env.dev')
 else:
-    ENV = dotenv_values('../.env')
-
-print('')
-
-# if __env_type == 'PROD':
-#     print('============ ENV PROD ===========')
-#     # ENV = dotenv_values('../envs/.env.prod')
-#     ENV = dotenv_values('../scraping-the-world/envs/.env.prod')
-# elif __env_type == 'HOMOLOG':
-#     print('============ ENV HOMOLOG ===========')
-#     ENV = dotenv_values('../envs/.env.homolog')
-# elif __env_type == 'DEV':
-#     print('============ ENV DEV ===========')
-#     # ENV = dotenv_values('../envs/.env.dev')
-#     ENV = dotenv_values('../scraping-the-world/envs/.env.dev')
-# else:
-#     ENV = dotenv_values('.')
+    if __env_type == 'PROD':
+        print('============ ENV PROD ===========')
+        ENV = dotenv_values('../envs/.env.prod')
+    elif __env_type == 'HOMOLOG':
+        print('============ ENV HOMOLOG ===========')
+        ENV = dotenv_values('../envs/.env.homolog')
+    elif __env_type == 'DEV':
+        print('============ ENV DEV ===========')
+        ENV = dotenv_values('../envs/.env.dev')
 
