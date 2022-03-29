6ea0fd8e142abeea1b2f3b759cf6f23891a9b9cd
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-29 17:02:01 -0300

    feat: change field title to titulo

--- scraping_the_world/models/database_schema.py
+++ scraping_the_world/models/database_schema.py
@@ -3,7 +3,7 @@ create table if not exists sites_data
 (
 	id int auto_increment
 		primary key,
-	title varchar(100) null,
+	titulo varchar(100) null,
 	preco varchar(50) null,
 	imagem varchar(50) null,
 	descricao varchar(500) null,
