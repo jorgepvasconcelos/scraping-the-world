a05d1064d193be2b60cf30b024f1ed1723e0f9e6
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-29 19:41:34 -0300

    test:  test_when_consult_americanas_without_description

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,4 +12,12 @@ def test_when_consult_americanas_with_description():
     assert valid_response == response
 
 
-
+def test_when_consult_americanas_without_description():
+    json_data = {'url': 'https://www.americanas.com.br/produto/3068486001'}
+    response = requests.get(url='http://localhost:5000/consult', json=json_data).json()
+    valid_response = {
+        'titulo': 'Smart TV 50" Crystal UHD Samsung 4k 50AU8000 Painel Dynamic Crystal Color Design Slim Tela Sem Limites',
+        'imagem': 'https://images-americanas.b2w.io/produtos/01/00/img/3068486/0/3068486027_1GG.jpg',
+        'preco': 'R$ 2.989,99', 'descricao': 'No Description',
+        'url': 'https://www.americanas.com.br/produto/3068486001'}
+    assert valid_response == response
