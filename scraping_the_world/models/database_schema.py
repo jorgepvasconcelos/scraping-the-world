b81bbe25f3fa2245fc98059216e4197adc1c3e94
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-29 10:11:53 -0300

    feat: update schema file

--- scraping_the_world/models/database_schema.py
+++ scraping_the_world/models/database_schema.py
@@ -3,13 +3,13 @@ create table if not exists sites_data
 (
 	id int auto_increment
 		primary key,
-	titulo varchar(100) null,
+	title varchar(100) null,
 	preco varchar(50) null,
 	imagem varchar(50) null,
 	descricao varchar(500) null,
-	url varchar(500) null,
-	data_verificado datetime default current_timestamp() null
+	url_recebida varchar(500) null,
+	data_verificado datetime default current_timestamp() null,
+	url_site varchar(500) null
 );
-
 
 """
