29ba296f746d7ab214125aac85830f332db04073
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-30 20:33:42 -0300

    feat: func get_config()

--- scraping_the_world/models/querys.py
+++ scraping_the_world/models/querys.py
@@ -3,8 +3,12 @@ import traceback
 from scraping_the_world.models.utils import DataBase
 
 
-def add_log(text: str, tipe: str):
+def add_log(text: str, tipe: str) -> None:
     try:
         DataBase.execute(query="INSERT INTO logs (text, tipe) VALUE (%s, %s)", arguments=[text, tipe])
     except:
         traceback.print_exc()
+
+
+def get_config(name: str):
+    return DataBase.consult_one(query="select value from configs where name = %s; ", arguments=[name])
