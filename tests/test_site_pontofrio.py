1e359e01d76858dcae528e44622bb2812ca16562
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-30 22:34:53 -0300

    test: new test test_when_consult_pontofrio_with_wrong_url_must_return_no_data

--- tests/test_site_pontofrio.py
+++ tests/test_site_pontofrio.py
@@ -1,17 +1,26 @@
+from pytest import mark, raises
 import requests
 
 from tests.helpers.helper import response_is_not_empty
 
 
-def test_when_consult_submarino_without_description_1():
+def test_when_consult_pontofrio_without_description_1():
     json_data = {'url': 'https://www.pontofrio.com.br/rack-136-madetec-lisboa-para-tv-ate-50/p/11996540'}
     response = requests.get(url='http://localhost:5000/consult', json=json_data).json()
 
     assert response_is_not_empty(response) == True
 
 
-def test_when_consult_submarino_without_description_2():
+def test_when_consult_pontofrio_without_description_2():
     json_data = {'url': 'https://www.pontofrio.com.br/refrigerador-electrolux-duplex-dc35a-260l-branco/p/1743666'}
     response = requests.get(url='http://localhost:5000/consult', json=json_data).json()
 
     assert response_is_not_empty(response) == True
+
+
+@mark.xfail()
+def test_when_consult_pontofrio_with_wrong_url_must_return_no_data():
+    json_data = {'url': 'https://www.pontofrio.com.br/544'}
+    response = requests.get(url='http://localhost:5000/consult', json=json_data).json()
+
+    assert response_is_not_empty(response) is True
