112ee068e6cc0b18583ffcb9c151d6c77fe64a7e
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-30 23:46:39 -0300

    feat: 'scraping_bubmarino', '1'

--- scraping_the_world/models/db_populate.py
+++ scraping_the_world/models/db_populate.py
@@ -1,3 +1,3 @@
 QUERY_POPULATE = ["insert into configs (name, value) values ('scraping_americanas', '1');",
                   "insert into configs (name, value) values ('scraping_pontofrio', '0');",
-                  "insert into configs (name, value) values ('scraping_bubmarino', '0');"]
+                  "insert into configs (name, value) values ('scraping_bubmarino', '1');"]
