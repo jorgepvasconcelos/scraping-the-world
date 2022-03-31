7490e9668cbd183a191950c96b819dc346489e7b
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-30 22:26:37 -0300

    test: new test test_when_consult_americanas_with_wrong_url_must_return_no_data

--- tests/test_site_americanas.py
+++ tests/test_site_americanas.py
@@ -1,5 +1,5 @@
+from pytest import mark, raises
 import requests
-
 from tests.helpers.helper import response_is_not_empty
 
 
@@ -7,11 +7,19 @@ def test_when_consult_americanas_with_description():
     json_data = {'url': 'https://www.americanas.com.br/produto/2896992161'}
     response = requests.get(url='http://localhost:5000/consult', json=json_data).json()
 
-    assert response_is_not_empty(response) == True
+    assert response_is_not_empty(response) is True
 
 
 def test_when_consult_americanas_without_description():
     json_data = {'url': 'https://www.americanas.com.br/produto/3068486001'}
     response = requests.get(url='http://localhost:5000/consult', json=json_data).json()
 
-    assert response_is_not_empty(response) == True
+    assert response_is_not_empty(response) is True
+
+
+@mark.xfail()
+def test_when_consult_americanas_with_wrong_url_must_return_no_data():
+    json_data = {'url': 'https://www.americanas.com.br/produto/30032022'}
+    response = requests.get(url='http://localhost:5000/consult', json=json_data).json()
+
+    assert response_is_not_empty(response) is True
