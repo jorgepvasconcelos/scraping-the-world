e4dc3efa1fe25930dcc25bee7533b825940a6e43
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-31 07:47:40 -0300

    feat: func path_to_file()

--- env.py
+++ env.py
@@ -1,32 +1,27 @@
+import os
+
 from dotenv import dotenv_values
 
-try:
-    __env_type = dotenv_values('.env')
-    __env_type = __env_type['ENV']
-    __ON_DOCKER = True
-except:
-    __env_type = dotenv_values('../.env')
-    __env_type = __env_type['ENV']
-    __ON_DOCKER = False
 
-if __ON_DOCKER:
-    if __env_type == 'PROD':
-        print('============ ENV PROD ===========')
-        ENV = dotenv_values('./envs/.env.prod')
-    elif __env_type == 'HOMOLOG':
-        print('============ ENV HOMOLOG ===========')
-        ENV = dotenv_values('./envs/.env.homolog')
-    elif __env_type == 'DEV':
-        print('============ ENV DEV ===========')
-        ENV = dotenv_values('./envs/.env.dev')
-else:
-    if __env_type == 'PROD':
-        print('============ ENV PROD ===========')
-        ENV = dotenv_values('../envs/.env.prod')
-    elif __env_type == 'HOMOLOG':
-        print('============ ENV HOMOLOG ===========')
-        ENV = dotenv_values('../envs/.env.homolog')
-    elif __env_type == 'DEV':
-        print('============ ENV DEV ===========')
-        ENV = dotenv_values('../envs/.env.dev')
+def path_to_file(file):
+    path = None
+    for root, dirs, files in os.walk('.'):
+        for name in files:
+            if name == file:
+                path = (os.path.abspath(os.path.join(root, name)))
 
+    return path
+
+
+__env_type = dotenv_values(path_to_file('.env'))
+__env_type = __env_type['ENV']
+
+if __env_type == 'PROD':
+    print('============ ENV PROD ===========')
+    ENV = dotenv_values(path_to_file('.env.prod'))
+elif __env_type == 'HOMOLOG':
+    print('============ ENV HOMOLOG ===========')
+    ENV = dotenv_values(path_to_file('.env.homolog'))
+elif __env_type == 'DEV':
+    print('============ ENV DEV ===========')
+    ENV = dotenv_values(path_to_file('.env.dev'))
