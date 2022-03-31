662863a09d427c87ba228d64fcbb310af570e3aa
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-03-30 21:17:27 -0300

    feat: file db_populate.py

---
+++ scraping_the_world/models/db_populate.py
@@ -0,0 +1,5 @@
+QUERY_POPULATE = """
+insert into configs (name, value) values ('scraping_americanas', '1');
+insert into configs (name, value) values ('scraping_pontofrio', '0');
+insert into configs (name, value) values ('scraping_bubmarino', '0');
+"""
